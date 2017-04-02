d512934603314c86b49b7e39d95117664a9850d9
Matthew Bavosa <matthew.bavosa@gmail.com>
2017-04-02 17:37:49 -0400

    starting javascript challenge

---
+++ js/main.js
@@ -0,0 +1,35 @@
+function(){
+var input = document.getElementById('usertext')
+
+input.addEventListener('keypress', eventHandler, false)
+
+var natoAlphabet = {
+    a:'alpha',
+    b:'bravo',
+    c:'charlie',
+    d:'delta',
+    e:'echo',
+    f:'foxtrot',
+    g:'golf',
+    h:'hotel',
+    i:'india',
+    j:'juliet',
+    k:'kilo',
+    l:'lima',
+    m:'mike',
+    n:'november',
+    o:'oscar',
+    p:'papa',
+    q:'quebec',
+    r:'romeo',
+    s:'sierra',
+    t:'tango',
+    u:'uniform',
+    v:'victor',
+    w:'whiskey',
+    x:'xray',
+    y:'yankee',
+    z:'zulu'
+  }
+
+}
